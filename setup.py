f7ee16bcf651dd3b7f8c965843ebe221ac6a260c
Anushreel <anushreelgowda12@gmail.com>
2024-01-24 19:38:21 +0530

    data ingestion

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPEN_E_DOT='-e .' #-e . automatically triggers setup.py
 def get_requirements(file_path:str)->List[str]:
     '''
     this will return list of requirments
@@ -9,13 +9,14 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]#reading each line and replacing \n with blank
     
         if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+            requirements.remove(HYPEN_E_DOT)#-e . will also be read that must be removed
         
     return requirements
-    
+
+#pip install -r requirements.txt
 
 setup(
     name='mlproject',
---
+++ src/components/data_ingestion.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass #used to create class variables
+
+# in dataingestion there shld be sm input 
+# any input will be given by data ingestion config
+@dataclass # decorator 
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered thhe data ingestion method or component")
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True) #saving inside artifact 
+            test_set.to_csv(self.ingestion_config.test_data_path,indeex=False,header=True)
+
+            logging.info("Ingestion of data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+        # read dataset frm url,api etc and coverted to csv and train_test split and v r returning train data path and test data path
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
